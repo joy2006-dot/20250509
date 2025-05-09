48ecd7797e3cbb434841822224c7e3811e18fbc8
joy2006-dot <s0982717742@gmail.com>
2025-05-09 10:29:26 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,15 @@ let video;
 let handPose;
 let hands = [];
 
+// 定義手部骨架結構
+const skeleton = [
+  [0, 1], [1, 2], [2, 3], [3, 4], // 大拇指
+  [0, 5], [5, 6], [6, 7], [7, 8], // 食指
+  [5, 9], [9, 10], [10, 11], [11, 12], // 中指
+  [9, 13], [13, 14], [14, 15], [15, 16], // 無名指
+  [13, 17], [17, 18], [18, 19], [19, 20] // 小指
+];
+
 function preload() {
   // Initialize HandPose model with flipped video input
   handPose = ml5.handPose({ flipped: true });
@@ -34,7 +43,17 @@ function draw() {
   if (hands.length > 0) {
     for (let hand of hands) {
       if (hand.confidence > 0.1) {
-        // Loop through keypoints and draw circles
+        // 繪製骨架
+        for (let [start, end] of skeleton) {
+          let startPoint = hand.keypoints[start];
+          let endPoint = hand.keypoints[end];
+
+          stroke(0, 255, 0);
+          strokeWeight(2);
+          line(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+        }
+
+        // 繪製關鍵點
         for (let i = 0; i < hand.keypoints.length; i++) {
           let keypoint = hand.keypoints[i];
 
